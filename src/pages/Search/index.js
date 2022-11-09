e4d3d2f1f15279a4ea11f10f7eff87c8ee997f11
Sean <millington.sean12@gmail.com>
2022-11-09 16:37:16 +0100

    search function almost works, error: name is undefined

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import { SearchForm } from '../../components'
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { SearchForm, Result } from '../../components'
+import { getResult } from '../../actions'
 
 const Search = () => {
+
+  const result = useSelector(state => state.result);
+  const username = useSelector(state => state.username);
+  const loading = useSelector(state => state.loading);
+  const error = useSelector(state => state.error)
+
+  const dispatch = useDispatch()
+
+  // console.log(result)
+  // console.log(username)
+
+  const search = searchTerm => dispatch(getResult(searchTerm))
+
+  const renderResult = () => loading ? <p>Loading...</p> : <Result result ={result}/>
+
   return (
-    
-    <SearchForm />
+    <>
+      <SearchForm getResult={search}/>
+
+      <h1>{username}</h1>
+
+      {error ? <p>there was an error {error}</p> : renderResult() }
+    </>
   )
 }
 
