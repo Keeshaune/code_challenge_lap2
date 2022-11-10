1957da437382c874a74ead006640441e48b2726f
“thamiem-n” <thamiem123@hotmail.com>
2022-11-10 14:28:59 +0000

    styling- need to fix positioning of div elements

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -8,20 +8,15 @@ export default function ({ data }) {
             <div className="user-data-positions">
                 <div>
                     <div className="header">
-                    <img src={`${data.avatar}`} />
-                    <h1>{data.name}</h1>
+                        <img src={`${data.avatar}`} />
+                        <h1>{data.name}</h1>
                     </div>
-                   
-                    
-                        <a href={`${data.link}`}>{data.link}</a>
-                   
+                    <a href={`${data.link}`}>{data.link}</a>
                 </div>
                 <div id="user-card">
-
                     <img src={`https://github-readme-stats.vercel.app/api?username=${data.name}&show_icons=true&locale=en&theme=transparent`} />
                 </div>
             </div>
-
             <div className="repo-container" >
                 {data.userRepos.map(repo => {
                     return <li key={Math.floor(Math.random() * 1000)}>{<Repo repo={repo} />}</li>
