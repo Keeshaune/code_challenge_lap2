cf30e5eb56400e77387942cdc481d527e71bfc50
“thamiem-n” <thamiem123@hotmail.com>
2022-11-10 21:38:09 +0000

    image added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-
-
+import './index.css'
 
 const Home = () => {
   return (
-    <> 
+    <div className="github-logo"> 
     
 
     <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Image of Github Logo" />
-    </>
+    </div>
   )
 }
 
